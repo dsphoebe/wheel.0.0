7e2d3bcbfcbba544226d942094b581d6402f51cd
dsphoebe <syh1217@qq.com>
2019-03-19 01:30:25 +0800

    use parcel bundler for vue files

---
+++ src/button.vue
@@ -0,0 +1,48 @@
+<template>
+  <button class="g-button"></button>
+</template>
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="scss">
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html {
+    --button-height: 32px;
+    --font-size: 14px;
+    --button-bg: white;
+    --button-active-bg: #eee;
+    --border-radius: 4px;
+    --color: #333;
+    --border-color: #999;
+    --border-color-hover: #666;
+  }
+
+  .g-button {
+    font-size: var(--font-size);
+    height: var(--button-height);
+    padding: 0 1em;
+    border-radius: var(--border-radius);
+    border: 1px solid var(--border-color);
+    background: var(--button-bg);
+    &:hover {
+      border-color: var(--border-color-hover);
+    }
+    &:active {
+      background-color: var(--button-bg-color-hover)
+    }
+    &:focus {
+      outline: none;
+    }
+  }
+  body {
+    font-size: var(--font-size);
+  }
+</style>
